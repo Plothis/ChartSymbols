95b6b6cecd503185559d70ec336bd61f84b000bb
neoddish <neoddish@outlook.com>
2020-12-05 23:53:39 +0800

    refactor(scripts): extract-svg (#3)
    * docs: update readme dev
    
    * refactor: extract-svg script

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ Symbols of Chart Types.
 ## Usage
 
 Check http://tuzhidian.com/
+
+## Development
+
+```bash
+npm install
+```
+
+```bash
+npm run build
+```
--- src/charts/treemap.ts
+++ src/charts/treemap.ts
@@ -1,60 +1,8 @@
-const treemap: string = `
-<svg width="240px" height="200px" viewBox="0 0 240 200" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-<!--Author: Plothis Studio - for Gradict - http://tuzhidian.com-->
-  <defs>
-    <style title="palette" type="text/css">
-      <![CDATA[
-        .color-theme-01 {
-          fill: #343e73;
-        }
-        .color-theme-02 {
-          fill: #22a6a1;
-        }
-        .color-theme-03 {
-          fill: #fa581f;
-        }
-        .color-theme-04 {
-          fill: #ff9c4d;
-        }
-        .color-theme-05 {
-          fill: #a6d169;
-        }
-        .color-theme-06 {
-          fill: #ffda8a;
-        }
-        .color-theme-07 {
-          fill: #e0b8ed;
-        }
-        .color-theme-08 {
-          fill: #d0215a;
-        }
-        .color-assist-bg {
-          fill: #ffffff;
-        }
-      ]]>
-    </style>
-  </defs>
-  <title>
-    Treemap
-  </title>
-  <g id="Treemap" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-    <g id="graphic" transform="translate(51.000000, 53.000000)" fill-rule="nonzero">
-      <rect id="Rectangle-08" class="color-theme-08" x="122" y="65" width="16" height="29"/>
-      <rect id="Rectangle-07" class="color-theme-07" x="100" y="65" width="22" height="29"/>
-      <rect id="Rectangle-06" class="color-theme-06" x="100" y="42" width="38" height="23"/>
-      <rect id="Rectangle-05" class="color-theme-05" x="70" y="42" width="30" height="52"/>
-      <rect id="Rectangle-04" class="color-theme-04" x="108" y="0" width="30" height="42"/>
-      <rect id="Rectangle-03" class="color-theme-03" x="70" y="0" width="38" height="42"/>
-      <rect id="Rectangle-02" class="color-theme-02" x="0" y="52" width="70" height="42"/>
-      <rect id="Rectangle-01" class="color-theme-01" x="0" y="0" width="70" height="52"/>
-    </g>
-  </g>
-</svg>
-`;
 
 const Treemap = {
   name: 'Treemap',
-  svgCode: treemap,
+  svgCode: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><g><path d="M101 34h54v59h-54z"/><path d="M3 34h98v73H3z"/><path d="M155 34h43v59h-43z"/><path d="M101 93h43v73h-43z"/><path d="M144 125h31v41h-31z"/><path d="M144 93h54v32h-54z"/><path d="M175 125h23v41h-23z"/><path d="M3 107h98v59H3z"/></g></svg>'
 };
 
 export default Treemap;
+      
