f80d4e6a9594a0792818e4be004a6b29b02b508d
jiazhewang <neoddish@outlook.com>
2020-12-22 23:41:23 +0800

    fix ts

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -1,13 +1,11 @@
 // generated file index.ts
 
 import donut_chart from './donut_chart';
-import heatmap from './heatmap';
-import line_chart from './line_chart';
 import radar_chart from './radar_chart';
 import sankey_diagram from './sankey_diagram';
 
 export const ChartSymbols = {
-  donut_chart, heatmap, line_chart, radar_chart, sankey_diagram
+  donut_chart, radar_chart, sankey_diagram
 };
 
-export { donut_chart, heatmap, line_chart, radar_chart, sankey_diagram };
+export { donut_chart, radar_chart, sankey_diagram };
