8fe6531ff5e4c0d70d2e74e60c7c35477a67bbe8
GitHub Action <action@github.com>
2020-12-29 14:10:31 +0000

    chore: Auto Update from SVGs

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -5,9 +5,10 @@ import heatmap from './heatmap';
 import line_chart from './line_chart';
 import radar_chart from './radar_chart';
 import sankey_diagram from './sankey_diagram';
+import treemap from './treemap';
 
 export const ChartSymbols = {
-  donut_chart, heatmap, line_chart, radar_chart, sankey_diagram
+  donut_chart, heatmap, line_chart, radar_chart, sankey_diagram, treemap
 };
 
-export { donut_chart, heatmap, line_chart, radar_chart, sankey_diagram };
+export { donut_chart, heatmap, line_chart, radar_chart, sankey_diagram, treemap };
